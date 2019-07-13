e8687521b75044345843774dbcb7d9df0611d7f5
anurag4516 <anurag4516@gmail.com>
2019-07-13 21:27:00 +0530

    Simple variable usage

--- k8objects/terraform.tf
+++ k8objects/terraform.tf
@@ -1,12 +1,16 @@
 provider "aws" {
     region ="us-east-1"
 }
+variable "server_port" {
+    description = "For specifying variable Port"
+    default = 8080
+}
 resource "aws_security_group" "terraform_sg"{
 name ="Security_Group_for_EC2_plus_Terraform"
 ingress {
 
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
