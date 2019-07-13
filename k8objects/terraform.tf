0af8cbd4bfc38071b1bb01bf235a3941764c5290
anurag4516 <anurag4516@gmail.com>
2019-07-13 21:01:34 +0530

    Adding Security Group Config

--- k8objects/terraform.tf
+++ k8objects/terraform.tf
@@ -1,6 +1,17 @@
 provider "aws" {
     region ="us-east-1"
 }
+resource "aws_security_group" "terraform_sg"{
+name ="Security_Group_for_EC2_plus_Terraform"
+ingress {
+
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+}
 resource "aws_instance" "terraform_demo_ec2"{
     ami ="ami-40d28157"
     instance_type="t2.micro"
