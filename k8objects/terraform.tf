6349990a792080b419bee1b199ca68950f71aa14
anurag4516 <anurag4516@gmail.com>
2019-07-14 00:12:51 +0530

    Adding SG in EC2

--- k8objects/terraform.tf
+++ k8objects/terraform.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region ="us-east-1"
+        region ="us-east-1"
 }
 variable "server_port" {
     description = "For specifying variable Port"
@@ -19,6 +19,7 @@ ingress {
 resource "aws_instance" "terraform_demo_ec2"{
     ami ="ami-40d28157"
     instance_type="t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.terraform_sg.id}"]
     tags {
         name = "terraform_demo_example"
     }
